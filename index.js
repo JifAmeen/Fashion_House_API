8c9c1062bdb13a3505c78f00d5520bb67d99da23
Jiffriya Ameen <121374769+JifAmeen@users.noreply.github.com>
2023-06-12 17:56:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "https://fashion-house.onrender.com/")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "https://fashion-house.onrender.com/", "build", "index.html"))
   );
 } else {
   // Default Route
